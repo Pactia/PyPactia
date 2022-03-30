c7efe0b25c8b99e5d3e28934454acdb453a6123a
monsadan <dmonsalve89@gmail.com>
2022-03-30 16:40:22 -0500

    [D ]First Commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='PyPactia',
+    version='0.0.1',
+    author='Analítica Pactia',
+    author_email='mike_huls@hotmail.com',
+    description='Paquete de utilidades de Pactia',
+    url='https://github.com/Pactia/PyPactia',
+    license='MIT',
+    packages=['PyPactia'],
+    install_requires=['requests'],
+)
